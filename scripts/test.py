f133bfaba9a6de32bb6385284e6c96498be2b6f5
Sarah Gibson <sgibson@turing.ac.uk>
2020-04-15 12:16:52 +0100

    [DEV] Remove local copy of UpgradeDockerTags as repo is now public

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import requests
-
-USERNAME = os.getenv("USER", None)
-API_TOKEN = os.getenv("API_TOKEN", None)
-
-response = requests.get('https://api.github.com/user', auth=(USERNAME, API_TOKEN))
-print(json.dumps(response.json(), indent=2, sort_keys=True))
